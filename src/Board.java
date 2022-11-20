52fa6f4d71a88319c835007ab7e955488c87a454
austinK123 <austin_kinnealey23@milton.edu>
2022-11-20 10:43:34 -0500

    updated code; working on winning boards logic and considering how to expand

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Board {
+
+    public ArrayList<Integer> boardList = new ArrayList<>();
+
+
+    public Board(int cola, int colb, int colc){
+        boardList.add(cola);
+        boardList.add(colb);
+        boardList.add(colc);
+    }
+
+}
--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+
 
 public class MyPlayer {
     public Chip[][] gameBoard;
@@ -8,40 +10,48 @@ public class MyPlayer {
     public int col2;
     public int col3;
 
+    boolean foundLoser = false;
+
+    public ArrayList<Board> losingBoard = new ArrayList<>();
+    public ArrayList<Board> winningBoard = new ArrayList<>();
+
     public MyPlayer() {
         columns = new int[10];
-
-        /***
-         * This code will run just once, when the game opens.
-         * Add your code here.
-         */
+        losingBoard.add(new Board(1, 0, 0));
         printBoards();
     }
-
     public Point move(Chip[][] pBoard) {
 
 
         System.out.println("MyPlayer Move");
 
         gameBoard = pBoard;
+
+        columns = checkChips(gameBoard);
+
         int column = 0;
         int row = 0;
 
         row = 1;
         column = 1;
 
-        /***
-         * This code will run each time the "MyPlayer" button is pressed.
-         * Add your code to return the row and the column of the chip you want to take.
-         * You'll be returning a data type called Point which consists of two integers.
-         */
-
         Point myMove = new Point(row, column);
         return myMove;
 
     }
-//100 200 300 110 210 310 220 320 330 111 211 311 221 321 331 222 322 332 333
+    public int[] checkChips(Chip[][] gameBoard) {
+        int[] cols = new int[10];
+        for(int c = 0; c < 10; c++){
+            for(int r = 0; r <10; r++){
+                if(gameBoard[r][c].isAlive == true){
+                    cols[c]++;
+                }
+            }
+        }
 
+
+        return cols;
+    }
     public void printBoards(){
         int boardnum = 1;
         for(int columnOne = 1; columnOne <= 3; columnOne++){
@@ -49,7 +59,6 @@ public class MyPlayer {
                 for(int columnThree = 0;columnThree <= columnTwo; columnThree++){
 
                         System.out.println(boardnum + " - board: " + columnOne + columnTwo + columnThree);
-
                         boardnum++;
 
                         col1 = columnOne;
@@ -62,39 +71,79 @@ public class MyPlayer {
         }
         System.out.println("***********************");
     }
+    public void printResultBoards(int a, int b, int c) {
 
-    public void printResultBoards(int a, int b, int c){
+        foundLoser = false;
 
-        for(int loopc = c-1; loopc >= 0; loopc --){
+        for (int loopc = c - 1; loopc >= 0; loopc--){
             System.out.println("resultboard: " + a + b + loopc);
-        }
 
-        for(int loopb = b-1; loopb >= 0; loopb --){
-            if(loopb < c){
+            for (Board lBoard : losingBoard) {
+                if (lBoard.boardList.get(0) == a && lBoard.boardList.get(1) == b && lBoard.boardList.get(2) == loopc) {
+                    System.out.println("found losing board");
+                    foundLoser = true;
+                }
+            }
+
+        }
+        for (int loopb = b - 1; loopb >= 0; loopb--) { // second col
+            if (loopb < c) {
                 System.out.println("resultboard: " + a + loopb + loopb);
-            }
-            else{
+                for (Board lBoard : losingBoard) {
+                    if (lBoard.boardList.get(0) == a && lBoard.boardList.get(1) == loopb && lBoard.boardList.get(2) == loopb) {
+                        System.out.println("found losing board");
+                        foundLoser = true;
+                    }
+                }
+            } else {
                 System.out.println("resultboard: " + a + loopb + c);
+                for (Board lBoard : losingBoard) {
+                    if (lBoard.boardList.get(0) == a && lBoard.boardList.get(1) == loopb && lBoard.boardList.get(2) == c) {
+                        System.out.println("found losing board");
+                        foundLoser = true;
+                    }
+                }
             }
         }
+        for(int loopa = a-1; loopa >= 1; loopa --){ // first col
+            if(loopa < b && loopa < c){
+                System.out.println("resultboard: " + loopa + loopa + loopa);
+                for (Board lBoard : losingBoard) {
+                    if (lBoard.boardList.get(0) == loopa && lBoard.boardList.get(1) == loopa && lBoard.boardList.get(2) == loopa){
+                        System.out.println("found losing board");
+                        foundLoser = true;
+                    }
+                }
 
-        for(int loopa = a-1; loopa >= 1; loopa --){
-            if(loopa < b || b < c){
-                System.out.println("resultboard: " + a + loopa + loopa);
+            }
+            else if(loopa < b){
+                System.out.println("resultboard: " + loopa + loopa + c);
+                for (Board lBoard : losingBoard) {
+                    if (lBoard.boardList.get(0) == loopa && lBoard.boardList.get(1) == loopa && lBoard.boardList.get(2) == c){
+                        System.out.println("found losing board");
+                        foundLoser = true;
+                    }
+                }
             }
             else{
                 System.out.println("resultboard: " + loopa + b + c);
+                for (Board lBoard : losingBoard) {
+                    if (lBoard.boardList.get(0) == loopa && lBoard.boardList.get(1) == b && lBoard.boardList.get(2) == c){
+                        System.out.println("found losing board");
+                        foundLoser = true;
+                    }
+                }
             }
         }
+
+
         System.out.println("**************************************************************************************************");
+
+        if(foundLoser == false){
+            losingBoard.add(new Board(a,b,c));
+        }
+        else{
+            winningBoard.add(new Board(a,b,c));
+        }
     }
-
-    public void winningBoards(){
-
-    }
-
 }
-
-
-
-
